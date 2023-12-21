7b641c7c97809e91da1843dc56ba12921e29a474
Ayo Onamusi <onamusia@gmail.com>
2023-12-21 13:05:06 +0000

    updated model to include id of the meal location

--- app/src/main/java/ie/setu/ayoeats/models/MealLocationMemStore.kt
+++ app/src/main/java/ie/setu/ayoeats/models/MealLocationMemStore.kt
@@ -5,14 +5,26 @@ import timber.log.Timber.Forest.i
 
 object MealLocationMemStore: AppCompatActivity(), MealLocationStore {
 
+    var lastId = 0L
+
+    internal fun getId(): Long {
+        return lastId++
+    }
+
     val mealLocations = ArrayList<MealLocationModel>()
     override fun create(mealLocation: MealLocationModel) {
+        mealLocation.uid = getId()
         mealLocations.add(mealLocation)
         logAll()
     }
 
     override fun findAll(): ArrayList<MealLocationModel> {
         return mealLocations
+    }
+
+    override fun findById(uid:Long) : MealLocationModel ? {
+        val foundMealLocation : MealLocationModel? = mealLocations.find {it.uid == uid}
+        return foundMealLocation
     }
 
     private fun logAll() {
