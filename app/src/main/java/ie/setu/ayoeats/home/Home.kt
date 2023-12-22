b0b9e7803e026c10570355115af948e24772fa0e
Ayo Onamusi <onamusia@gmail.com>
2023-12-22 23:50:25 +0000

    updated the firebase auth to MVMM

--- app/src/main/java/ie/setu/ayoeats/home/Home.kt
+++ app/src/main/java/ie/setu/ayoeats/home/Home.kt
@@ -1,5 +1,6 @@
 package ie.setu.ayoeats.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
@@ -11,29 +12,46 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseUser
 import ie.setu.ayoeats.R
 import ie.setu.ayoeats.databinding.ActivityMainBinding
+import ie.setu.ayoeats.databinding.NavHeaderMainBinding
+import ie.setu.ayoeats.ui.auth.LoggedInViewModel
+import ie.setu.ayoeats.ui.auth.Login
 
 class Home : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var homeBinding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var loggedInViewModel: LoggedInViewModel
+    private lateinit var navHeaderBinding : NavHeaderMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // binding to main activity
+        homeBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        drawerLayout = homeBinding.drawerLayout
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+//        homeBinding.appBarMain.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+
+        val navView: NavigationView = homeBinding.navView
+        navView.setupWithNavController(navController)
+
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -42,7 +60,29 @@ class Home : AppCompatActivity() {
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        loggedInViewModel = ViewModelProvider(this).get(LoggedInViewModel::class.java)
+        loggedInViewModel.liveFirebaseUser.observe(this, Observer { firebaseUser ->
+            if (firebaseUser !=null)
+                updateNavHeader(loggedInViewModel.liveFirebaseUser.value!!)
+        })
+
+        // if the user is logged out
+        loggedInViewModel.loggedOut.observe(this, Observer { loggedout ->
+            if(loggedout) {
+                startActivity(Intent(this, Login::class.java))
+            }
+        })
+    }
+
+    private fun updateNavHeader(currentUser: FirebaseUser){
+        var headerView = homeBinding.navView.getHeaderView(0)
+        navHeaderBinding = NavHeaderMainBinding.bind(headerView)
+        navHeaderBinding.navHeaderEmail.text = currentUser.email
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
