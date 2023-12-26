711fc722657a1924e3a4c1e19db3e7e32da07bf8
Ayo Onamusi <onamusia@gmail.com>
2023-12-26 04:23:03 +0000

    Getting other user's meal locations and displaying them in the maps fragment

--- app/src/main/java/ie/setu/ayoeats/ui/map/MapsFragment.kt
+++ app/src/main/java/ie/setu/ayoeats/ui/map/MapsFragment.kt
@@ -6,11 +6,20 @@ import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -76,6 +85,7 @@ class MapsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         loader = createLoader(requireActivity())
+        setupMenu()
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
@@ -88,9 +98,15 @@ class MapsFragment : Fragment() {
 
     // render for meal locations
     private fun render(mealLocationsList: ArrayList<MealLocationModel>) {
+        var markerColour : Float
         if (!mealLocationsList.isEmpty()) {
             mapsViewModel.map.clear()
             mealLocationsList.forEach {
+              markerColour = if(it.email.equals(this.mealLocationListViewModel.liveFirebaseUser.value!!.email))
+                  BitmapDescriptorFactory.HUE_AZURE + 5
+                else
+                    BitmapDescriptorFactory.HUE_RED
+
                 mapsViewModel.map.addMarker(
                     MarkerOptions().position(LatLng(it.latitude, it.longitude))
                         .title("${it.mealName} €${it.mealDescription}")
@@ -112,6 +128,34 @@ class MapsFragment : Fragment() {
                 mealLocationListViewModel.load()
             }
         }
+    }
+
+    // Menu
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onPrepareMenu(menu: Menu) {
+                // Handle for example visibility of menu items
+            }
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main, menu)
+
+                val item = menu.findItem(R.id.toggleDonations) as MenuItem
+                item.setActionView(R.layout.togglebutton_layout)
+                val toggleDonations: SwitchCompat = item.actionView!!.findViewById(R.id.toggleButton)
+                toggleDonations.isChecked = false
+
+                toggleDonations.setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) mealLocationListViewModel.loadAll()
+                    else mealLocationListViewModel.load()
+                }
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Validate and handle the selected menu item
+                return NavigationUI.onNavDestinationSelected(menuItem,
+                    requireView().findNavController())
+            }     }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
 
